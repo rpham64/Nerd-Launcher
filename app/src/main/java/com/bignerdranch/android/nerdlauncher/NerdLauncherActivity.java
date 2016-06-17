8bc13cf979c34feb973d91f0fa4f1a7c098ce8b2
Rudolf Pham <rpham64@gmail.com>
2016-06-17 13:25:14 -0700

    feature1

--- app/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherActivity.java
+++ app/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherActivity.java
@@ -3,7 +3,7 @@ package com.bignerdranch.android.nerdlauncher;
 import android.support.v4.app.Fragment;
 
 /**
- *
+ * feature1
  */
 public class NerdLauncherActivity extends SingleFragmentActivity {
 
