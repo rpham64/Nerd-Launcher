b28e63dc01b19844aad27e4fdbaedf84563be28d
Rudolf Pham <rpham64@gmail.com>
2016-06-17 13:21:48 -0700

    idk

--- app/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherActivity.java
+++ app/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherActivity.java
@@ -2,6 +2,9 @@ package com.bignerdranch.android.nerdlauncher;
 
 import android.support.v4.app.Fragment;
 
+/**
+ * TEst123
+ */
 public class NerdLauncherActivity extends SingleFragmentActivity {
 
     @Override
